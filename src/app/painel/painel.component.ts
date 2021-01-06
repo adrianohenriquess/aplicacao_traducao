56a9db32b0d447ba4644a446778dfa70700df585
adrianohenriquess <adriano.henriquesds@gmail.com>
2021-01-06 18:14:25 -0300

    Finalizando a regra para decrementar os corações para cada tentativa

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,4 +1,7 @@
+import { ProgressoComponent } from './../progresso/progresso.component';
+import { Frase } from './../shared/frase.model';
 import { Component, OnInit } from '@angular/core';
+import { FRASES } from './frases-mock';
 
 @Component({
   selector: 'app-painel',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PainelComponent implements OnInit {
 
-  constructor() { }
+  public frases: Frase[] = FRASES;
+  public instrucao: string = "Traduza a frase:";
+  public resposta: string = "";
+  public rodada: number = 0;
+  public rodadaFrase!: Frase;
+  public progresso: number = 0;
+  public tentativas: number = 3;
+
+
+  constructor() { 
+    this.atualizaRodada();
+  }
 
   ngOnInit(): void {
   }
 
+  public atualizaResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value;
+  }
+
+  public verificarResposta(): void {
+    if (this.rodadaFrase.frasePtBr == this.resposta) {
+      this.rodada++;
+      this.progresso = this.progresso + (100 / this.frases.length);
+      this.atualizaRodada();
+    } else {
+      this.tentativas--;
+      if (this.tentativas === -1) {
+        alert('Voce perdeu todas as tentativas');
+      }
+    }
+  }
+
+  public atualizaRodada(): void {
+    this.rodadaFrase = this.frases[this.rodada];
+    this.resposta = '';
+  }
 }
--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -1,18 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Coracao } from './../shared/coracao.model';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tentativas',
   templateUrl: './tentativas.component.html',
   styleUrls: ['./tentativas.component.css']
 })
-export class TentativasComponent implements OnInit {
+export class TentativasComponent implements OnInit, OnChanges {
 
-  public coracaoVazio: string = '/assets/coracao_vazio.png';
-  public coracaoCheio: string = '/assets/coracao_cheio.png';
+  @Input() public tentativas!: number;
+
+  public coracoes: Array<Coracao> = [
+    new Coracao(true),
+    new Coracao(true),
+    new Coracao(true)
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngOnChanges(): void {
+    if (this.tentativas !== this.coracoes.length) {
+      let indice = this.coracoes.length - this.tentativas;
+      this.coracoes[indice - 1].cheio = false;
+    }
+  }
 }
